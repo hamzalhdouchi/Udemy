b7b710a17c088a9dfb01a2aefca445dce42213d4
hamzalhadouchi <hamzabniss16@gmail.com>
2025-01-17 22:40:03 +0000

    Déclaration des propriétés de la classe Database

---
+++ app/config/database.php
@@ -0,0 +1,39 @@
+<?php
+
+class Database
+{
+    private $localHost = 'localhost';
+    private $name = 'root';
+    private $password = '';
+    private $dbname = 'udemy';
+    private $connect;
+
+    private static $instance = null;
+
+    private function __construct()
+    {
+        try {
+            $dns = "mysql:host={$this->localHost};dbname={$this->dbname};charset=utf8";
+            $this->connect = new PDO($dns, $this->name, $this->password);
+            $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            echo 'Erreur de connexion : ' . $e->getMessage();
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connect;
+    }
+
+}
+
